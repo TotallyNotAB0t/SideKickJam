ed62a6b41fa60a656ade80cc897873f167ce5853
KindiLuv <anykennylster@gmail.com>
2022-05-14 16:43:15 +0200

    SKJ flipped player model

--- SidekickJamProject/Assets/Scripts/PlayerMovement.cs
+++ SidekickJamProject/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,16 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         float velocity = Input.GetAxis("Horizontal");
+        if (velocity > 0 && !facesRight)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            facesRight = !facesRight;
+        }
+        else if (velocity < 0 && facesRight)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            facesRight = !facesRight;
+        }
         transform.Translate(speed * velocity * 0.2f, 0f, 0f);
     }
 }
